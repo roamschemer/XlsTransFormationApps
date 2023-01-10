8c3ff7b8a8e00dd7a3488117aedea79b3a3f7f61
roamschemer <roamschemer@gmail.com>
2023-01-11 01:27:22 +0900

    プロジェクト ファイルを追加します。

---
+++ XlsTransFormationApps/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+
+string xslt = @"<?xml version='1.0'?>
+    <xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform' version='1.0'>
+        <xsl:template match='/Parent'>
+            <Root>
+                <C1>
+                <xsl:value-of select='Child1'/>
+                </C1>
+                <C2>
+                <xsl:value-of select='Child2'/>
+                </C2>
+            </Root>
+        </xsl:template>
+    </xsl:stylesheet>";
+
+var oldDocument = new XDocument(
+    new XElement("Parent",
+        new XElement("Child1", "Child1 data"),
+        new XElement("Child2", "Child2 data")
+    )
+);
+
+var newDocument = new XDocument();
+
+using (var stringReader = new StringReader(xslt)) {
+    using (XmlReader xsltReader = XmlReader.Create(stringReader)) {
+        var transformer = new XslCompiledTransform();
+        transformer.Load(xsltReader);
+        using (XmlReader oldDocumentReader = oldDocument.CreateReader()) {
+            using (XmlWriter newDocumentWriter = newDocument.CreateWriter()) {
+                transformer.Transform(oldDocumentReader, newDocumentWriter);
+            }
+        }
+    }
+}
+
+string result = newDocument.ToString();
+Console.WriteLine(result);
