b2b2fd264972aa3042dbe01edbf8816cd65afc3d
roamschemer <roamschemer@gmail.com>
2023-01-11 22:03:16 +0900

    変換ファイルを出力

--- XlsTransFormationApps/Program.cs
+++ XlsTransFormationApps/Program.cs
@@ -1,41 +1,27 @@
-﻿using System.Xml;
+﻿using System.Text;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Xsl;
 
-string xslt = @"<?xml version='1.0'?>
-    <xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform' version='1.0'>
-        <xsl:template match='/Parent'>
-            <Root>
-                <C1>
-                <xsl:value-of select='Child1'/>
-                </C1>
-                <C2>
-                <xsl:value-of select='Child2'/>
-                </C2>
-            </Root>
-        </xsl:template>
-    </xsl:stylesheet>";
+var oldDocument = XDocument.Load("old.xml");
 
-var oldDocument = new XDocument(
-    new XElement("Parent",
-        new XElement("Child1", "Child1 data"),
-        new XElement("Child2", "Child2 data")
-    )
-);
+var xslt = "";
+using (var sr = new StreamReader("trans.xslt")) {
+    xslt = sr.ReadToEnd();
+}
 
 var newDocument = new XDocument();
 
 using (var stringReader = new StringReader(xslt)) {
-    using (XmlReader xsltReader = XmlReader.Create(stringReader)) {
-        var transformer = new XslCompiledTransform();
-        transformer.Load(xsltReader);
-        using (XmlReader oldDocumentReader = oldDocument.CreateReader()) {
-            using (XmlWriter newDocumentWriter = newDocument.CreateWriter()) {
-                transformer.Transform(oldDocumentReader, newDocumentWriter);
-            }
-        }
-    }
+    using var xsltReader = XmlReader.Create(stringReader);
+    var transformer = new XslCompiledTransform();
+    transformer.Load(xsltReader);
+    using var oldDocumentReader = oldDocument.CreateReader();
+    using var newDocumentWriter = newDocument.CreateWriter();
+    transformer.Transform(oldDocumentReader, newDocumentWriter);
 }
 
 string result = newDocument.ToString();
 Console.WriteLine(result);
+newDocument.Save("new.xml");
+
